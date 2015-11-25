cd57120d1ca2e93714288721c8a5a1999c44bf3f
DrPengPeng <58268636@qq.com>
2015-11-25 23:34:49 +0800

    写了normalizer的东西 又用了super agent 总体还是一个感觉：坑 今天写不动了

--- app/components/DataGridComponent.jsx
+++ app/components/DataGridComponent.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react'
 import { Table, Checkbox } from 'antd';
-
+import { Row, Col, Input } from 'antd';
+import request from 'superagent';
+import { normalize, Schema, arrayOf } from 'normalizr';
 
 const columns = [{
     colSpan: 0,
@@ -12,51 +14,54 @@ const columns = [{
     }
 },{
     colSpan:0,
-    title: '姓名',
-    dataIndex: 'name',
+    title: '任务文本',
+    dataIndex: 'text',
     render: function(text) {
         return <a href="javascript:;">
             {text}
         </a>;
     }
 }];
-const data = [{
-  key: '1',
-  name: '胡彦斌'
-}, {
-  key: '2',
-  name: '胡彦祖'
-}, {
-  key: '3',
-  name: '李大嘴'
-}];
-
-// 通过 rowSelection 对象表明需要行选择
-const rowSelection = {
-  type: "radio",
-  getCheckboxProps: function(record) {
-    return {
-      defaultChecked: record.name === '李大嘴', // 配置默认勾选的列
-      disabled: record.name === '胡彦祖'    // 配置无法勾选的列
-    };
-  },
-  onSelect: function(record, selected, selectedRows) {
-    console.log(record, selected, selectedRows);
-  },
-  onSelectAll: function(selected, selectedRows) {
-    console.log(selected, selectedRows);
-  }
-};
 
 const DataGridComponent = React.createClass({
+    getInitialState() {
+        this.handleAsyncRequest();
+        return {};
+        // let data = [];
+        // let returnData = request
+        // .get('/jsons/quests.json')
+        // .end(function(err, res){
+        //     let fuck = res.body.data;
+        //     // let shit = normalize(fuck, {data: arrayOf(new Schema('data'))})
+        //     data = normalize(fuck, arrayOf(new Schema('data')))
+        //     console.log(data);
+        //     return {data};
+        // });
+    },
     render () {
         return (
             <div>
-                <Table
-                    columns={columns}
-                    dataSource={data}
-                    pagination={false}
-                     />
+                <Row type="flex" justify="center">
+                    <Col span="23">
+                        <Input size="large" placeholder="添加任务..." />
+                    </Col>
+                </Row>
+                <Row type="flex" justify="center">
+                    <Col span="23">
+                        <br/>
+                        <hr/>
+                    </Col>
+                </Row>
+                <Row type="flex" justify="center">
+                    <Col span="23">
+                        <Table
+                            columns={columns}
+                            dataSource={[]}
+                            pagination={false}
+                            />
+                    </Col>
+                </Row>
+
             </div>
         )
     }
--- app/containers/InboxContainer.jsx
+++ app/containers/InboxContainer.jsx
@@ -1,26 +1,11 @@
 import React, { PropTypes } from 'react'
-import { Row, Col, Input } from 'antd';
 import DataGridComponent from '../components/DataGridComponent.jsx'
 
 const InboxContainer = React.createClass({
     render () {
         return (
             <div>
-                <Row type="flex" justify="center">
-                    <Col span="23">
-                        <Input size="large" placeholder="添加任务..." />
-                    </Col>
-                </Row>
-                <Row type="flex" justify="center">
-                    <Col span="23">
-                        <br/><hr/>
-                    </Col>
-                </Row>
-                <Row type="flex" justify="center">
-                        <Col span="23">
-                            <DataGridComponent/>
-                        </Col>
-                </Row>
+                <DataGridComponent />
             </div>
         )
     }
