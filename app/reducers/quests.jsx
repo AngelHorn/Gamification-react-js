a602eb66fc849abd756646bd2e82573fbed0e278
DrPengPeng <58268636@qq.com>
2015-12-12 15:50:25 +0800

    修复CRUD时排序的BUG

--- app/reducers/quests.jsx
+++ app/reducers/quests.jsx
@@ -1,27 +1,31 @@
 import * as actionTypes from '../actions/actionTypes.jsx';
 
 function quests(state = [], action) {
-  let newState, questIndex;
+  let newState
+  let questIndex
   switch (action.type) {
-       case actionTypes.RECEIVE_QUESTS:
-         return action.quests
-       case actionTypes.COMPLETE_QUEST:
-         newState = state.concat();
-         questIndex = newState.findIndex((quest) => quest.id === action.id);
-         newState[questIndex].state = 1;
-         return newState;
-         break;
-     case actionTypes.CANCEL_COMPLETE_QUEST:
-       newState = state.concat();
-       questIndex = newState.findIndex((quest) => quest.id === action.id);
-       newState[questIndex].state = 0;
-       return newState;
-       break;
-       case actionTypes.ADD_QUEST:
-         newState = state.concat();
-         newState.push(action.newQuest);
-         return newState;
-         break;
+    case actionTypes.COMPLETE_QUEST:
+      questIndex = state.findIndex((quest) => quest.id === action.id);
+      return [
+        ...state.slice(0, questIndex),
+        Object.assign({}, state[questIndex], {
+          state: 1
+        }),
+        ...state.slice(questIndex + 1)
+      ];
+    case actionTypes.CANCEL_COMPLETE_QUEST:
+      questIndex = state.findIndex((quest) => quest.id === action.id);
+      return [
+        ...state.slice(0, questIndex),
+        Object.assign({}, state[questIndex], {
+          state: 0
+        }),
+        ...state.slice(questIndex + 1)
+      ];
+    case actionTypes.ADD_QUEST:
+      return [ action.newQuest, ...state ];
+    case actionTypes.RECEIVE_QUESTS:
+      return action.quests.reverse();
     default:
       return state;
   }
